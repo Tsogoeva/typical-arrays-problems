4dd89797cd6288b4c92cf8bebcdd4712963a1650
Tsogoeva <tdagger@mail.ru>
2021-03-12 17:23:15 +0300

    feat: solution of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  };
+
+  const arrSort = array.sort((a, b) => a - b);
+  return arrSort[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  };
+
+  const arrSort = array.sort((a, b) => a - b);
+  return arrSort[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  };
+
+  const arrSum = array.reduce((sum, item) => sum + item, 0);
+  return arrSum / array.length;
 }
